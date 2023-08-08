ea5017b10507acfecc5d92ccbcc82a8348f4ccb6
Jordan Victoria <jvictoria2484@gmail.com>
2023-08-08 14:24:29 -0700

    deleted some code in java for mounth day and year

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ $(function () {
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+    // current hour in 24-hour time?\
 
-    var today = dayjs();
-    $('#1a').text(today.format('MMM D, YYYY'));
-    
+
+
+
+
 
 
 
